3862fe86758f21c61e4c937d80e5aeb945bb1845
Keith Rutkowski <keith@analytech-solutions.com>
2021-07-19 13:27:11 -0400

    Use CBinding 1.0

--- src/P4est.jl
+++ src/P4est.jl
@@ -1,6 +1,5 @@
 module P4est
 
-# using CBinding: @cstruct
 using Reexport: @reexport
 
 include("libp4est.jl")
--- src/libp4est.jl
+++ src/libp4est.jl
@@ -1,21 +1,40 @@
-# @reexport baremodule LibP4est
 @reexport module LibP4est
-  # using CBinding: 𝐣𝐥
-  using MPI: MPI_Datatype, MPI_Comm, MPI_File
+  using CBinding
+  import P4est_jll
   
-  # Introduce standard integer types
-  # const size_t   = 𝐣𝐥.Csize_t
-  # const ssize_t  = 𝐣𝐥.Cssize_t
-  # const int8_t   = 𝐣𝐥.Int8
-  # const int16_t  = 𝐣𝐥.Int16
-  # const int32_t  = 𝐣𝐥.Int32
-  # const int64_t  = 𝐣𝐥.Int64
-  # const uint64_t = 𝐣𝐥.UInt64
-
-  # Forward declare standard library types
-  # 𝐣𝐥.@cstruct FILE
-  # 𝐣𝐥.@cstruct va_list
-
-  # 𝐣𝐥.Base.include((𝐣𝐥.@__MODULE__), 𝐣𝐥.joinpath(𝐣𝐥.dirname(𝐣𝐥.@__DIR__), "deps", "libp4est.jl"))
-  include(joinpath(dirname(@__DIR__), "deps", "libp4est.jl"))
+  let
+    p4est_library = P4est_jll.libp4est_path
+    p4est_include = joinpath(dirname(dirname(P4est_jll.libp4est_path)), "include")
+    p4est_library_dir = joinpath(dirname(dirname(P4est_jll.libp4est_path)), "lib")
+    c`-I$p4est_include -L$p4est_library_dir -lp4est`
+  end
+  
+  const c"int8_t" = Int8
+  const c"int16_t" = Int16
+  const c"int32_t" = Int32
+  const c"int64_t" = Int64
+  const c"uint8_t" = UInt8
+  const c"uint16_t" = UInt16
+  const c"uint32_t" = UInt32
+  const c"uint64_t" = UInt64
+  const c"size_t" = Csize_t
+  const c"ssize_t" = Cssize_t
+  
+  # define them as Cvoid since they are usually used as opaque
+  const c"FILE"    = Cvoid
+  const c"va_list" = Cvoid
+  
+  c"""
+    #include <stdio.h>
+    #include <stdarg.h>
+  """s
+  
+  c"""
+    #include "p4est.h"
+    #include "p4est_extended.h"
+    #include "p6est.h"
+    #include "p6est_extended.h"
+    #include "p8est.h"
+    #include "p8est_extended.h"
+  """ji
 end
